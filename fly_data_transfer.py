3482a603318f730da96416403ad264439017beec
Yong Tan <yong.tan@flex.com>
2019-05-22 21:26:50 +0800

    add test ca8 file update the fly_data_transfer for some update

--- fly_data_transfer.py
+++ fly_data_transfer.py
@@ -2,11 +2,89 @@
 import os,  subprocess, time, sys
 currentDir = os.getcwd()
 #import the base py file
+import re
 sys.path.append(currentDir +"/Common")
 
+
+class TransferFile:
+    def __init__(self, fileName=None):
+	self._setting = {} 
+	if fileName == None:
+	    return
+	try:
+	    self._file = open(fileName, "r")
+	except IOError, exception:
+	    print "Can't open the configuratin file: ", exception
+	    sys.exit(1)
+	    
+	#self.Read()
+    def Read(self):
+	lines = self._file.readlines()
+        print len(lines)
+        cos=''
+        i=1
+        for line in lines:
+
+            if line.find('//')==0 or line.find('@')==0:
+                continue
+            if i>10:
+                break
+            #print line.split()
+            if line.split():
+                name = line.split()[0]
+                Om = line.split()[1]
+                data_re = re.compile(r'([1-9]\d*\.?\d*)|(0\.\d*[1-9])')
+                data_m = data_re.search(Om)
+                Om_data=0
+                if data_m:
+                    Om_data=data_m.group()
+                
+                start1 = line.split()[2]
+                start2 = line.split()[3]
+                r= line.split()[4]
+                if r!='R': 
+                    x1= line.split()[5]
+                    y1= line.split()[6]
+                    x2= line.split()[7]
+                    y2= line.split()[8]
+                    atT = line.split()[9]
+                    ten1 = line.split()[10]
+                    ten2 = line.split()[11]
+                else:
+                    x1= line.split()[5]
+                    y1= line.split()[6]
+                    x2= line.split()[7]
+                    y2= line.split()[8]
+                    atT = line.split()[9]
+                    ten1 = line.split()[10]
+                li_cop='WCOP,    %s, %s,  %s,   %s,  %s,--------,--------,N,N,N,0,0,0' %(name,x1,y1,x2,y2)
+                li_cos='WCOS,    %s,%s 1-2      ,  , ,00,00,   0,   0,%.3E,%.3E,  0.0 [%%],  0.0 [%%],1,0,0,0,0,0.000,0.000E+000,   0,   0,0,0,0,00,0.000E+000' %(i,name,float(Om_data),float(Om_data))
+                print line
+                print li_cop
+                print li_cos
+                i=i+1
+                    #ten2 = line.split()[11]
+	#while line != '':
+	#    print line
+	#while line != '':
+	#    print line
+	#    if line[0][0] != '/' and line != '\n':
+	#	line2 = line.split('\t')
+	#	line2 = filter(self.IsNotNull, line2)
+		#print line2
+		#name = line2[0].strip()
+		#value = line2[1].strip()
+		#comment = line2[2].strip()
+		#self._setting[name] = value
+	#    line = self._file.readline()
+
+
 if __name__=="__main__":
+    transfer_file ='LSB1GP24D_bot.ca8'
     while True:
         if True:
+            if transfer_file:
+                break
             #cre=RFWIFIIC(1)
             #cre.ScanData()
             #cre.Wait(cre.wait_time)
@@ -36,11 +114,13 @@ if __name__=="__main__":
             if transfer_type=="q":
                break
 
-        try:
-            #open the file
-            pass
-        except:
-            pass
+    tfer = TransferFile(transfer_file)
+    tfer.Read()
+    try:
+         #open the file
+         pass
+    except:
+         pass
             #print "Error"
         #for amb_index in [0,1,4]:
         #    write_str=write_str+str(cre.amb_sensores["amb%s_read_raw_data" %amb_index])+","
